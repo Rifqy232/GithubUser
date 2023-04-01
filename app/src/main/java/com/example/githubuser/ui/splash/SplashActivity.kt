8d1a97daffbc5a45244f5a52ccc7748c9e6b80fa
Rifqy232 <rachmanmry20d@student.unhas.ac.id>
2023-04-02 07:18:54 +0800

    Add splash screen

---
+++ app/src/main/java/com/example/githubuser/ui/splash/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.githubuser.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.githubuser.R
+import com.example.githubuser.ui.home.HomeActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
